d5653f7a86492d6b3031e6a4919b8f9a3501d925
Hager Ramadan <hagerramadan440@gmail.com>
2025-09-08 20:55:56 +0300

    update route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './components/home/home';
 import { Header } from './components/header/header';
-import { Search } from './components/search/search';
-import { MovieCard } from './components/movie-card/movie-card';
+
 
 @Component({
   selector: 'app-root',
