ad5ea4624651b680ba5f62c953704176451bd012
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-09-08 20:37:17 +0300

    update static page

--- src/app/components/wishlist/wishlist.ts
+++ src/app/components/wishlist/wishlist.ts
@@ -14,28 +14,28 @@ export class Wishlist {
       date: 'Sep 25, 2017',
       rating: 9288,
       description: 'Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by...',
-      poster: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnOT8UPTx6xk84_gQIsBuesYHSvcuy6sa0wisMo7zeeJl1Xw3UfewVZQnO-WICSt4eRZx7iFbuXe2tj52N85uPPiW2-srhao4rrI6T49tcnEruvYxo97UJaC_c_Qgekoghuk1XByyPoFqVvEY9vQFxmqVNEcGZAtSqnAF6kMSvIaXqUpOgj2px_cObNSS7xz_zFm33fNhiGtbFcQEILMdV0Y459ITbtVGnWAsxp7WXxW3yHT4-XTIC5OD6tyZyZDSuEtpYXyIXLj3Y',
+      poster: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqjJ87P-OJLGPvTnWZ4RrVsYDv3lLdO_Gbt38cbW6U8bIhazqE',
     },
     {
       title: 'Black Widow',
       date: 'Sep 25, 2017',
       rating: 9288,
       description: 'Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by...',
-      poster: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnOT8UPTx6xk84_gQIsBuesYHSvcuy6sa0wisMo7zeeJl1Xw3UfewVZQnO-WICSt4eRZx7iFbuXe2tj52N85uPPiW2-srhao4rrI6T49tcnEruvYxo97UJaC_c_Qgekoghuk1XByyPoFqVvEY9vQFxmqVNEcGZAtSqnAF6kMSvIaXqUpOgj2px_cObNSS7xz_zFm33fNhiGtbFcQEILMdV0Y459ITbtVGnWAsxp7WXxW3yHT4-XTIC5OD6tyZyZDSuEtpYXyIXLj3Y',
+      poster: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqjJ87P-OJLGPvTnWZ4RrVsYDv3lLdO_Gbt38cbW6U8bIhazqE',
     },
     {
       title: 'Black Widow',
       date: 'Sep 25, 2017',
       rating: 9288,
       description: 'Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by...',
-      poster: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnOT8UPTx6xk84_gQIsBuesYHSvcuy6sa0wisMo7zeeJl1Xw3UfewVZQnO-WICSt4eRZx7iFbuXe2tj52N85uPPiW2-srhao4rrI6T49tcnEruvYxo97UJaC_c_Qgekoghuk1XByyPoFqVvEY9vQFxmqVNEcGZAtSqnAF6kMSvIaXqUpOgj2px_cObNSS7xz_zFm33fNhiGtbFcQEILMdV0Y459ITbtVGnWAsxp7WXxW3yHT4-XTIC5OD6tyZyZDSuEtpYXyIXLj3Y',
+      poster: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqjJ87P-OJLGPvTnWZ4RrVsYDv3lLdO_Gbt38cbW6U8bIhazqE',
     },
     {
       title: 'Black Widow',
       date: 'Sep 25, 2017',
       rating: 9288,
       description: 'Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by...',
-      poster: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnOT8UPTx6xk84_gQIsBuesYHSvcuy6sa0wisMo7zeeJl1Xw3UfewVZQnO-WICSt4eRZx7iFbuXe2tj52N85uPPiW2-srhao4rrI6T49tcnEruvYxo97UJaC_c_Qgekoghuk1XByyPoFqVvEY9vQFxmqVNEcGZAtSqnAF6kMSvIaXqUpOgj2px_cObNSS7xz_zFm33fNhiGtbFcQEILMdV0Y459ITbtVGnWAsxp7WXxW3yHT4-XTIC5OD6tyZyZDSuEtpYXyIXLj3Y',
+      poster: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqjJ87P-OJLGPvTnWZ4RrVsYDv3lLdO_Gbt38cbW6U8bIhazqE',
     },
   ];
 }
