8042b4092bb164a0add2ec5c2b0ebdf9b4d76e0c
Hager Ramadan <hagerramadan440@gmail.com>
2025-09-09 16:41:19 +0300

    add snackbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal , HostListener  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header , MatSnackBarModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  showButton: boolean = false;
   protected readonly title = signal('movieApp');
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.showButton = window.scrollY > 300;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
