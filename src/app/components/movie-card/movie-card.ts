5e1ca2d605069b03e439cdb84c4811f507e1547a
MMarzoo <mmarzouk12378@gmail.com>
2025-09-10 16:05:21 +0300

    feat(movie): implement movie search functionality and improve movie card display

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe, NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  imports: [DatePipe, DecimalPipe, NgClass],
   templateUrl: './movie-card.html',
-  styleUrl: './movie-card.css'
+  styleUrl: './movie-card.css',
 })
 export class MovieCard {
+  @Input() movie: any;
+  isFavorite: boolean = false;
 
+  constructor(private router: Router) {}
+
+  getPoster(movie: any) {
+    return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+  }
 }
