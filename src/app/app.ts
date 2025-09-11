7f264ce08ca21e1da56418e8adac40b70383023e
Mohamed Yasser <mohamedgamedeveloper@gmail.com>
2025-09-11 04:37:42 -0700

    2

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./components/home/home";
 import { Header } from "./components/header/header";
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , Header],
+  imports: [RouterOutlet , Header, FormsModule,BrowserModule,HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
