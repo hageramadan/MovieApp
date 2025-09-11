637cd12161788091c0bbe9c8cec8e33846cd18cc
MMarzoo <mmarzouk12378@gmail.com>
2025-09-11 17:20:36 +0300

    update   add to watch list

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -2,6 +2,9 @@ import { DatePipe, DecimalPipe, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Watchlist } from '../../shared/watchlist';
+import { UserCredtionalI } from '../../shared/user-credtional-i';
+
 @Component({
   selector: 'app-movie-card',
   imports: [DatePipe, DecimalPipe, NgClass],
@@ -14,18 +17,46 @@ export class MovieCard {
   @Input() popularMovies?: any[] = [];
   @Input() topRatedMovies?: any[] = [];
   @Input() upcomingMovies?: any[] = [];
+
   isFavorite: boolean = false;
-  constructor(private snackBar: MatSnackBar, private router: Router) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private watchListService: Watchlist,
+    private user: UserCredtionalI
+  ) {}
   getPoster(movie: any) {
     return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
   }
 
   toggleFavorite() {
+    if (!this.movie) return;
+
     this.isFavorite = !this.isFavorite;
+
+    const accountId = this.user.accountId;
+    const sessionId = this.user.sessionId;
+    const mediaType = 'movie';
+
+    if (this.isFavorite) {
+      this.watchListService
+        .addtoWatchList(mediaType, this.movie.id, <string>accountId, <string>sessionId)
+        .subscribe({
+          next: () => this.openSnackBar('Added to watchlist'),
+          error: (err) => console.error('Error adding:', err),
+        });
+    } else {
+      this.watchListService
+        .removefromWatchList(mediaType, this.movie.id, <string>accountId, <string>sessionId)
+        .subscribe({
+          next: () => this.openSnackBar('Removed from watchlist'),
+          error: (err) => console.error('Error removing:', err),
+        });
+    }
   }
 
-  openSnackBar() {
-    this.snackBar.open('Saved to watchlist', 'X', {
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'X', {
       duration: 3000, // مدة الظهور بالمللي ثانية
       horizontalPosition: 'right', // start | center | end | left | right
       verticalPosition: 'top',
