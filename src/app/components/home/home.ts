25e339ae7d087201e4fee9b046703b227ced680d
MMarzoo <mmarzouk12378@gmail.com>
2025-09-12 01:50:34 +0300

    update search query in home and search pages

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,28 +1,29 @@
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from '../movie-card/movie-card';
 import { Movie } from '../../shared/services/movie';
-import { PaginationComponent } from "../movie-pagination/movie-pagination";
+import { PaginationComponent } from '../movie-pagination/movie-pagination';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgbModule, MovieCard, PaginationComponent],
+  imports: [CommonModule, NgbModule, MovieCard, PaginationComponent, FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class HomeComponent implements OnInit {
-
+  searchQuery: string = '';
   movies: any = {
     trending: { data: [], page: 1, totalPages: 0 },
     popular: { data: [], page: 1, totalPages: 0 },
     topRated: { data: [], page: 1, totalPages: 0 },
-    upcoming: { data: [], page: 1, totalPages: 0 }
+    upcoming: { data: [], page: 1, totalPages: 0 },
   };
 
-  constructor(private movieService: Movie) {}
+  constructor(private movieService: Movie, private router: Router) {}
 
   ngOnInit(): void {
     this.loadMovies('trending', 1);
@@ -36,7 +37,7 @@ export class HomeComponent implements OnInit {
 
     switch (category) {
       case 'trending':
-        request$ = this.movieService.getTrendingMovies( page);
+        request$ = this.movieService.getTrendingMovies(page);
         break;
       case 'popular':
         request$ = this.movieService.getPopularMovies(page);
@@ -51,7 +52,7 @@ export class HomeComponent implements OnInit {
         return;
     }
 
-    request$.subscribe(data => {
+    request$.subscribe((data) => {
       this.movies[category].data = data.results;
       this.movies[category].page = data.page;
       this.movies[category].totalPages = data.total_pages;
@@ -61,6 +62,12 @@ export class HomeComponent implements OnInit {
   onPageChange(category: string, page: number): void {
     this.loadMovies(category, page);
   }
+
+  onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], {
+        queryParams: { q: this.searchQuery },
+      });
+    }
+  }
 }
-
-
