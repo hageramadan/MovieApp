210b4262219d16d6e7b01dc609f68c773c5fb4f8
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-09-11 22:12:56 +0300

    create pagination component
    I create  component  to paginate each section in home component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,44 +1,66 @@
+
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovieCard } from "../movie-card/movie-card";
 import { Movie } from '../../shared/services/movie';
+import { PaginationComponent } from "../movie-pagination/movie-pagination";
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgbModule, MovieCard],
+  imports: [CommonModule, NgbModule, MovieCard, PaginationComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent implements OnInit {
-  trendingMovies: any[] = [];
-  popularMovies: any[] = [];
-  topRatedMovies: any[] = [];
-  upcomingMovies: any[] = [];
 
-  constructor(private Movie: Movie) {}
+  movies: any = {
+    trending: { data: [], page: 1, totalPages: 0 },
+    popular: { data: [], page: 1, totalPages: 0 },
+    topRated: { data: [], page: 1, totalPages: 0 },
+    upcoming: { data: [], page: 1, totalPages: 0 }
+  };
+
+  constructor(private movieService: Movie) {}
 
   ngOnInit(): void {
-    // Trending
-    this.Movie.getTrendingMovies('day').subscribe(data => {
-      this.trendingMovies = data.results;
-    });
-
-    // Popular
-    this.Movie.getPopularMovies().subscribe(data => {
-      this.popularMovies = data.results;
-    });
-
-    // Top Rated
-    this.Movie.getTopRatedMovies().subscribe(data => {
-      this.topRatedMovies = data.results;
-    });
-
-    // Upcoming
-    this.Movie.getUpcomingMovies().subscribe(data => {
-      this.upcomingMovies = data.results;
-    });
-    
+    this.loadMovies('trending', 1);
+    this.loadMovies('popular', 1);
+    this.loadMovies('topRated', 1);
+    this.loadMovies('upcoming', 1);
   }
- 
+
+  loadMovies(category: string, page: number): void {
+    let request$;
+
+    switch (category) {
+      case 'trending':
+        request$ = this.movieService.getTrendingMovies( page);
+        break;
+      case 'popular':
+        request$ = this.movieService.getPopularMovies(page);
+        break;
+      case 'topRated':
+        request$ = this.movieService.getTopRatedMovies(page);
+        break;
+      case 'upcoming':
+        request$ = this.movieService.getUpcomingMovies(page);
+        break;
+      default:
+        return;
+    }
+
+    request$.subscribe(data => {
+      this.movies[category].data = data.results;
+      this.movies[category].page = data.page;
+      this.movies[category].totalPages = data.total_pages;
+    });
+  }
+
+  onPageChange(category: string, page: number): void {
+    this.loadMovies(category, page);
+  }
 }
+
+
--- src/app/components/movie-pagination/movie-pagination.ts
+++ src/app/components/movie-pagination/movie-pagination.ts
@@ -1,65 +1,21 @@
-// import { Component, Input, OnInit } from '@angular/core';
-// import { MovieModel, MovieApiResponse } from '../../models/move';
-// import { Movie } from '../../shared/services/movie';
+import { Component, Input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-// @Component({
-//   selector: 'app-movie-pagination',
-//   templateUrl: './movie-pagination.component.html',
-//   styleUrls: ['./movie-pagination.component.css']
-// })
-// export class MoviePaginationComponent implements OnInit {
-//   @Input() sectionType: 'trending' | 'popular' | 'topRated' | 'upcoming' = 'popular';
+@Component({
+  selector: 'app-movie-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-pagination.html',
+  styleUrls: ['./movie-pagination.css']
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  pageChange = output<number>();
 
-//   movies: Movie[] = [];
-//   currentPage = 1;
-//   totalPages = 0;
-
-//   constructor(private movieService: Movie) {}
-
-//   ngOnInit(): void {
-//     this.loadMovies(this.currentPage);
-//   }
-
-//   loadMovies(page: number) {
-//     switch (this.sectionType) {
-//       case 'trending':
-//         this.movieService.getTrendingMovies('day').subscribe((res: MovieApiResponse) => {
-//           this.movies = res.results;
-//           this.totalPages = res.total_pages;
-//         });
-//         break;
-//       case 'popular':
-//         this.movieService.getPopularMovies(page).subscribe((res: MovieApiResponse) => {
-//           this.movies = res.results;
-//           this.totalPages = res.total_pages;
-//         });
-//         break;
-//       case 'topRated':
-//         this.movieService.getTopRatedMovies(page).subscribe((res: MovieApiResponse) => {
-//           this.movies = res.results;
-//           this.totalPages = res.total_pages;
-//         });
-//         break;
-//       case 'upcoming':
-//         this.movieService.getUpcomingMovies(page).subscribe((res: MovieApiResponse) => {
-//           this.movies = res.results;
-//           this.totalPages = res.total_pages;
-//         });
-//         break;
-//     }
-//   }
-
-//   nextPage() {
-//     if (this.currentPage < this.totalPages) {
-//       this.currentPage++;
-//       this.loadMovies(this.currentPage);
-//     }
-//   }
-
-//   prevPage() {
-//     if (this.currentPage > 1) {
-//       this.currentPage--;
-//       this.loadMovies(this.currentPage);
-//     }
-//   }
-// }
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+      this.pageChange.emit(page);
+    }
+  }
+}
