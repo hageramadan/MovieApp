e2ea988ceb717484cbafb008b00047bb51005539
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-09-12 09:15:46 +0300

    and css and static data

--- src/app/components/account-details/account-details.ts
+++ src/app/components/account-details/account-details.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccountDetailsI } from '../../models/account-details-i';
 
 @Component({
   selector: 'app-account-details',
-  imports: [],
   templateUrl: './account-details.html',
-  styleUrl: './account-details.css'
+  styleUrls: ['./account-details.css'],
+  imports: [CommonModule]
 })
 export class AccountDetails {
+  public account: AccountDetailsI = {
+    id: 123456,
+    name: "John Doe",
+    username: "johndoe123",
+    iso_639_1: "en",
+    iso_3166_1: "US",
+    avatar: {
+      gravatar: {
+        hash: "d4c5b6a7890f1e2d3c4b5a6f7890e1f2" // Example Gravatar hash
+      }
+    }
+  };
+  logout()
+  {
 
+  }
+  // constructor() {} // Not needed for static data
 }
---
+++ src/app/models/account-details-i.ts
@@ -0,0 +1,12 @@
+export interface AccountDetailsI {
+  id: number;
+  name: string;
+  username: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  avatar: {
+    gravatar: {
+      hash: string;
+    };
+  };
+}
